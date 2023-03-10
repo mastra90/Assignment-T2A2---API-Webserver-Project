9790b1a6def2aa9b6de9d9f94a9470e22fc9a027
mastra90 <lukejmastrocola@gmail.com>
2023-03-10 23:30:26 +1100

    initial setup

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return """
+    <html>
+        <head>
+            <title>Welcome to Movie DB</title>
+        </head>
+        <body style="background-color: #131410;">
+            <h1 style="color: #F5F6ED; text-align: center;">Welcome to Movie DB</h1>
+        </body>
+    </html>
+    """
