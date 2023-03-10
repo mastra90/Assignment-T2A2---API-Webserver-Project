04a7bc48e94ab334ff859dad6ba869eafe6236c9
mastra90 <lukejmastrocola@gmail.com>
2023-03-10 23:45:51 +1100

    second commit to test

--- app.py
+++ app.py
@@ -7,10 +7,11 @@ def index():
     return """
     <html>
         <head>
-            <title>Welcome to Movie DB</title>
+            <title>Welcome to Movie DB test</title>
         </head>
         <body style="background-color: #131410;">
             <h1 style="color: #F5F6ED; text-align: center;">Welcome to Movie DB</h1>
         </body>
     </html>
     """
+
