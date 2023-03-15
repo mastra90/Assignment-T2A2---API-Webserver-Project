a2ec09a4d3caa2cba453e40d0ff3af54558e43af
mastra90 <lukejmastrocola@gmail.com>
2023-03-15 19:53:04 +1100

    Created movies Class

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+#establish connection
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://postgres:@localhost:5432/box_office_db"
+db = SQLAlchemy(app)
+
+class Movies(db.Model):
+    # tablename
+    __tablename__ = "movies"
+    # Primary key
+    movie_id = db.Column(db.Serial(), primary_key=True)
+    # additional attributes
+    title = db.Column(db.String())
+    genre = db.Column(db.String())
+    year_released = db.Column(db.Integer)
+    runtime = db.Column(db.Interval())
+    rotten_tomatoes_rating = db.Column(db.Integer)
+
 
 @app.route("/")
 def index():
